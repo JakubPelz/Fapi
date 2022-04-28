c0976bc078b04cc2139bd717a358f522fb927e7d
Jakub Pelz <jakubpelz@gmail.com>
2022-04-28 18:58:27 +0200

    EURdone

--- frontend/src/components/finalPageComponents/ForeignCurrency.js
+++ frontend/src/components/finalPageComponents/ForeignCurrency.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const ForeignCurrency = () => {
+  const [eur, setEur] = useState('');
+
+  const URL = 'http://localhost:5000/';
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(URL);
+      setEur(data);
+    })();
+  }, []);
+  console.log(eur);
   return <div>ForeignCurrency</div>;
 };
 
