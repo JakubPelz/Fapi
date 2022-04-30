e980458a4276b16b8e08f9f66dc7d2abe2e20213
Jakub Pelz <jakubpelz@gmail.com>
2022-04-30 16:02:12 +0200

    Final commit

--- frontend/src/components/inputs/EmailInput.js
+++ frontend/src/components/inputs/EmailInput.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const EmailInput = ({ email, setEmail, user }) => {
+  const emailValidation = () => {
+    const regexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email === '') {
+      return <div></div>;
+    }
+    if (!email.toLowerCase().match(regexp)) {
+      return <div className="validation">Email není ve správném tvaru.</div>;
+    } else {
+      return <div></div>;
+    }
+  };
   return (
     <div className="field">
       <label>Email</label>
@@ -11,6 +23,7 @@ const EmailInput = ({ email, setEmail, user }) => {
         id="email"
         onChange={(e) => setEmail(e.target.value)}
       />
+      {emailValidation()}
     </div>
   );
 };
--- frontend/src/components/inputs/NameInput.js
+++ frontend/src/components/inputs/NameInput.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const NameInput = ({ user, name, setName }) => {
   const nameValidation = () => {
+    const regexp = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+
     if (name === '') {
       return <div></div>;
     }
-    if (name.trim().length === 5) {
+    if (name.match(regexp)) {
       return <div></div>;
     } else {
-      return <div className="validation">Jméno a příjmení jsou 2 slova</div>;
+      return <div className="validation">Zadáváte neplatné znaky</div>;
     }
   };
   return (
